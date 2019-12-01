496d22208d543b78240da995a9f6257c284c1c63
Kenny Choi <kkenc.gg@gmail.com>
2019-12-01 21:40:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
-var theFunk = funkyFunction().get:function();
+var theFunk = funkyFunction.get:function();
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
