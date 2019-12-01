058cd546b609b4c347087c02f2e0053333c8ec1c
Kenny Choi <kkenc.gg@gmail.com>
2019-12-01 21:27:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
+//variable should be on top
+const two = 2
 var animal = 'dog'
+
 
 function myAnimal() {
   return animal
 }
+/*
+* not sure how you want me to code this
+*/
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  
+  // hard coding cat animal = "cat";
+  // promt user for his/her animal var userAnimal = window.prompt("What is your animal: ");
+  //
   return animal
 }
 
 function add2(n) {
   return n + two
 
-  // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
+  
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction;
