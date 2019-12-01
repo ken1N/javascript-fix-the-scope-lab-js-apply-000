3db62e4b0874b14581b5161c97e8aa9819ef2525
Kenny Choi <kkenc.gg@gmail.com>
2019-12-01 21:36:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,11 +28,10 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
+  
   return function() {
     return "FUNKY!"
-    
   }
-      
 }
 var theFunk = funkyFunction();
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
